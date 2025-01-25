d429775f07d17439033d76ad42eff1a0fbbae200
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-01-25 14:28:06 -0600

    starting to phase out manual classifier for DORA_XGB

--- scripts/00_run_example_pks_bio.py
+++ scripts/00_run_example_pks_bio.py
@@ -1,8 +1,8 @@
 from biosynth_pipeline import biosynth_pipeline
+from DORA_XGB import DORA_XGB
 import os
 import pickle
 import warnings
-
 warnings.simplefilter('ignore')
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
@@ -12,20 +12,17 @@ pathway_sequence = ['pks','bio']  # choose between ['pks'] or ['pks','bio']
 target_smiles = 'CCCCCCC'
 target_name = 'heptane'
 pks_release_mechanism = 'thiolysis' # choose from 'cyclization' or 'thiolysis'
-feasibility_cofactors = '../data/coreactants_and_rules/all_cofactors_updated.csv'
 
 config_filepath = 'input_config_file.json'
 
-### Create an object that is an instance of the feasibility classification model
-PX = biosynth_pipeline.feasibility_classifier(ML_model_type = 'add_concat',
-                                              cofactors_path = feasibility_cofactors)
+post_pks_rxn_model = DORA_XGB.feasibility_classifier()
 
 ### Create an object that is an instance of Biosynth Pipeline
 biosynth_pipeline_object = biosynth_pipeline.biosynth_pipeline(
                                              pathway_sequence = pathway_sequence,
                                              target_smiles = target_smiles,
                                              target_name = target_name,
-                                             feasibility_classifier = PX,
+                                             feasibility_classifier = post_pks_rxn_model,
                                              pks_release_mechanism = pks_release_mechanism,
                                              config_filepath = config_filepath)
 
